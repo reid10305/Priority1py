b6fea76a0af4bbe4b92df47de42c7b0a52ce0bb1
Daniel Pifer <daniel.pifer13@gmail.com>
2023-06-14 19:50:02 -0500

    new release for troubleshooting

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'Priority1py',         # How you named your package folder (MyLib)
   packages = ['Priority1py'],   # Chose the same as "name"
-  version = '0.1.1-beta',      # Start with a small number and increase it with every change you make
+  version = '0.1.2-beta',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Priority 1 web API python library',   # Give a short description about your library
   author = 'Daniel Pifer',                   # Type in your name
@@ -12,8 +12,7 @@ setup(
   download_url = 'https://github.com/reid10305/Priority1py/archive/refs/tags/v0.1.0-beta.tar.gz',    # I explain this later on
   keywords = ['API', 'Priority1'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'requests',
-          'enum'
+          'requests'
       ],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
