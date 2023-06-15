1fdb56223f51adecb233bd13a4e684b95b079846
Daniel Pifer <daniel.pifer13@gmail.com>
2023-06-14 20:26:04 -0500

    kms

--- Priority1py/__init__.py
+++ Priority1py/__init__.py
@@ -1,3 +1,3 @@
 from Priority1py import Priority1py
 from request import req_send
-from strings import Endpoint, Crud, Accessorial, Identifiers
+from strings import Endpoint, Crud, Accessorial, IDType
