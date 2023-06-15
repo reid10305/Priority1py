6900e1dccd93354682f59f530ddde67145a7cb5a
Daniel Pifer <daniel.pifer13@gmail.com>
2023-06-14 20:45:07 -0500

    almost there

--- Priority1py/strings.py
+++ Priority1py/strings.py
@@ -2,16 +2,22 @@
 from enum import Enum
 
 class Endpoint(Enum):
+    def __str__(self):
+        return self.value
     LTL_RATE_QUOTE = '/v2/ltl/quotes/rates'
     LTL_SHIPMENT_STATUS = '/v2/ltl/shipments/status'
 
 
 class Crud(Enum):
+    def __str__(self):
+        return self.value
     POST = 'POST'
     GET = 'GET'
 
 
 class Accessorial(Enum):
+    def __str__(self):
+        return self.value
     GUARANTEED_5PM = 'GUR'
     DELIV_APPT = 'APPT'
     LIFT_GATE_DELIV = 'LGDEL'
@@ -20,6 +26,8 @@ class Accessorial(Enum):
 
 
 class IDType(Enum):
+    def __str__(self):
+        return self.value
     PRO = 'PRO'
     BOL = 'BILL_OF_LADING'
     CUST_REF = 'CUSTOMER_REFERENCE'
